a09803b8a6a9526dc172bfab55d243d1d154e029
Hlidskjalf <vroman197@gmail.com>
2017-10-18 12:29:53 -0400

    Exit button icon example

--- src/GUI_Examples/Button.java
+++ src/GUI_Examples/Button.java
@@ -2,17 +2,18 @@ package GUI_Examples;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class Button extends JPanel implements ActionListener {
 
     protected JButton b1;
+    protected JLabel button;
 
-    public Button() {
+    public Button() throws IOException {
 
         //TODO Create a series of buttons (7) to replicate the Util program, and an exit btn
 
@@ -23,6 +24,18 @@ public class Button extends JPanel implements ActionListener {
         b1.setHorizontalTextPosition(AbstractButton.LEADING);
         b1.setMnemonic(KeyEvent.VK_B);
         b1.setActionCommand("button1");
+
+        JLabel button = new JLabel();
+        button.setIcon(new ImageIcon("res/exitbutton.png"));
+
+        button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                JOptionPane.showInputDialog("Exit Button Clicked");
+            }
+        });
+
 
         /**
          * To build out the FCE-UTIL Program, we will need buttons for:
@@ -43,6 +56,7 @@ public class Button extends JPanel implements ActionListener {
 
         // Add Buttons to be displayed
         add(b1);
+        add(button);
 
     }
 
@@ -57,7 +71,7 @@ public class Button extends JPanel implements ActionListener {
         }
     }
 
-    protected static void app() {
+    protected static void app() throws IOException {
         JFrame frame = new JFrame("Buttons");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -70,6 +84,7 @@ public class Button extends JPanel implements ActionListener {
 
         Button newContentPane = new Button();
         newContentPane.setOpaque(true);
+        newContentPane.setBackground(Color.decode("0x3B3F42"));
         frame.setContentPane(newContentPane);
 
         frame.pack();
--- src/GUI_Examples/Main.java
+++ src/GUI_Examples/Main.java
@@ -1,12 +1,18 @@
 package GUI_Examples;
 
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                Button.app();
+                try {
+                    Button.app();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
